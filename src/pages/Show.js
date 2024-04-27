bd0bad55f104653f5316662d10f7b151afebb1e6
Alex Greenberg <acgreenberg2@gmail.com>
2024-04-27 11:07:01 -0400

    actions are updated and in routes

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -3,8 +3,19 @@ import { useLoaderData } from "react-router-dom"
 const ShowOne = (props) => {
     const product = useLoaderData()
     return (
+        <div>
+            <div className="header-container">
+                
+            </div>
+            <h4>{product.name}</h4>
+            <img src={product.image} width={300} alt="product image"></img>
+            <div className="container">
+                <h4>Size: {product.size} // </h4>
+                <h4>Price: ${product.price}</h4>
+            </div>
+
+        </div>
         
-        <h1>{product.name}</h1>
     )
 }
 
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Update from "./pages/Update";
 import ShowOne from "./pages/Show";
 import Create from "./pages/Create"
 import { productLoader, showLoader } from "./loader";
+import { createAction, updateAction, deleteAction } from "./actions";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -12,7 +13,10 @@ const router = createBrowserRouter(
             <Route path="" element={<Index/>} loader={productLoader}/>
             <Route path=":id" element={<ShowOne/>} loader={showLoader}/>
             <Route path=":id/update" element={<Update/>} loader={showLoader}/>
-            <Route path="new" element={<Create/>}/>           
+            <Route path="new" element={<Create/>}/>
+            <Route path="create" action={createAction}/>
+            <Route path="update/:id" action={updateAction}/>
+            <Route path="delete/:id" action={deleteAction}/>         
         </Route>
     )
     
